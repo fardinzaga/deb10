b9ff533e736d166a320d86ede51fc61cf7b073ae
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2022-07-14 10:37:03 +0700

    Update install-ws.sh

--- lain2/install-ws.sh
+++ lain2/install-ws.sh
@@ -10,7 +10,7 @@ wget -O /usr/local/bin/ws-bintang https://raw.githubusercontent.com/fardinzaga/d
 wget -O /usr/local/bin/ws-multi https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/multi-ws.py
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/openssh-ws.py
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/ovpn-ws.py
-wget -O /usr/local/bin/ws-fauzanvpn https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/fauzanvpn-ws.py
+wget -O /usr/local/bin/ws-pastibisa https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/pastibisa-ws.py
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/ws-ovpn.py && chmod +x /usr/local/bin/ws-ovpn.py
 
 #izin permision
@@ -19,7 +19,7 @@ chmod +x /usr/local/bin/ws-bintang
 chmod +x /usr/local/bin/ws-multi
 chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-ovpn
-chmod +x /usr/local/bin/ws-fauzanvpn
+chmod +x /usr/local/bin/ws-pastibisa
 chmod +x /usr/local/bin/ws-ovpn
 
 #System OpenSSH Websocket-SSH Python
@@ -38,7 +38,7 @@ wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com
 wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/service-wsovpn && chmod +x /etc/systemd/system/ws-ovpn.service
 
 #System OpenSSH Websocket-SSH Python
-wget -O /etc/systemd/system/ws-fauzanvpn.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service-wsfauzanvpn && chmod +x /etc/systemd/system/ws-fauzanvpn.service
+wget -O /etc/systemd/system/ws-pastibisa.service https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/service-wspastibisa && chmod +x /etc/systemd/system/ws-pastibisa.service
 
 #System Websocket-OpenVPN Python
 wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service.wsovpn && chmod +x /etc/systemd/system/ws-ovpn.service
@@ -56,25 +56,25 @@ systemctl enable ws-bintang.service
 systemctl start ws-bintang.service
 systemctl restart ws-bintang.service
 
-#Enable & Start & Restart ws-maulana service
+#Enable & Start & Restart ws-multi service
 systemctl enable ws-multi.service
 systemctl start ws-multi.service
 systemctl restart ws-multi.service
 
-#Enable & Start & Restart ws-zahara service
+#Enable & Start & Restart ws-openssh service
 systemctl enable ws-openssh.service
 systemctl start ws-openssh.service
 systemctl restart ws-openssh.service
 
-#Enable & Start & Restart ws-dropbear service
+#Enable & Start & Restart ws-ovpn service
 systemctl enable ws-ovpn.service
 systemctl start ws-ovpn.service
 systemctl restart ws-ovpn.service
 
-#Enable & Start & Restart ws-fauzanvpn service
-systemctl enable ws-fauzanvpn.service
-systemctl start ws-fauzanvpn.service
-systemctl restart ws-fauzanvpn.service
+#Enable & Start & Restart ws-pastibisa service
+systemctl enable ws-pastibisa.service
+systemctl start ws-pastibisa.service
+systemctl restart ws-pastibisa.service
 
 #Enable & Start ws-ovpn service
 systemctl enable ws-ovpn.service
