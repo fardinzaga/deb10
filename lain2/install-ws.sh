f7c7ba7934840b70cca6284069310f2a6a41f1b9
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2022-07-23 12:48:18 +0700

    Update install-ws.sh

--- lain2/install-ws.sh
+++ lain2/install-ws.sh
@@ -6,7 +6,6 @@ cd
 
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/dropbear-ws.py
-wget -O /usr/local/bin/ws-bintang https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/bintang-ws.py
 wget -O /usr/local/bin/ws-julian https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/julian-ws.py
 wget -O /usr/local/bin/ws-multi https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/multi-ws.py
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/openssh-ws.py
@@ -14,7 +13,6 @@ wget -O /usr/local/bin/ws-pastibisa https://raw.githubusercontent.com/fardinzaga
 
 #izin permision
 chmod +x /usr/local/bin/ws-dropbear
-chmod +x /usr/local/bin/ws-bintang
 chmod +x /usr/local/bin/ws-julian
 chmod +x /usr/local/bin/ws-multi
 chmod +x /usr/local/bin/ws-openssh
@@ -22,9 +20,6 @@ chmod +x /usr/local/bin/ws-pastibisa
 
 #System OpenSSH Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
-
-#System OpenSSH Websocket-SSH Python
-wget -O /etc/systemd/system/ws-bintang.service https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/service-wsbintang && chmod +x /etc/systemd/system/ws-bintang.service
 
 #System Websocket-OpenVPN Python
 wget -O /etc/systemd/system/ws-julian.service https://raw.githubusercontent.com/fardinzaga/deb10/master/websocket/service-wsjulian && chmod +x /etc/systemd/system/ws-julian.service
@@ -45,11 +40,6 @@ wget -O /etc/systemd/system/ws-pastibisa.service https://raw.githubusercontent.c
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
 systemctl restart ws-dropbear.service
-
-#Enable & Start & Restart ws-bintang service
-systemctl enable ws-bintang.service
-systemctl start ws-bintang.service
-systemctl restart ws-bintang.service
 
 #Enable & Start ws-julian service
 systemctl enable ws-julian.service
